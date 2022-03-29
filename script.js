565fd2e5094ba177c407c9fcdf443106e2d9626f
lifa-karuniawati <101052257+lifa-karuniawati@users.noreply.github.com>
2022-03-29 17:16:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const numbers = document.querySelectorAll(".number")
 
+numbers.forEach((number) => {
+    number.addEventListener("click",(event) =>{
+        console.log(event.target.value)
+    })
+})
